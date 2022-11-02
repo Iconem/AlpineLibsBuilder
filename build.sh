f5c21f5603ae4163bd87ddbfe97b03c59c595f6c
caroline ramond <caroline.ramond@iconem.com>
2022-11-02 11:17:22 +0100

    added comments in build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
+# clean the build folder
 rm -r build
 mkdir build
+
+# build alpine image containing the executables
 docker build -t alpine-libs-builder .
+
+# launch a container containing the executables
 PID=$(docker run -itd alpine-libs-builder)
+
+# fetch the container's executables into build folder
 docker cp "$PID:/home/lib/PNPSolver/build/camCalibNode" ./build
 docker cp "$PID:/home/lib/PotreeConverter1.7/build/PotreeConverter/PotreeConverter" ./build/PotreeConverter1.7
 docker cp "$PID:/home/lib/PotreeConverter2.1/build/PotreeConverter" ./build/PotreeConverter2.1
+
+# clean the container
 docker stop "$PID"
 docker rm "$PID"
